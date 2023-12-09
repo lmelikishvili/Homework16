c2521afde26cea7085ed837ab58c7fa8b8aaccfd
lmelikishvili <levanimelikishvili@gmail.com>
2023-12-10 02:37:53 +0400

    before_navigation

---
+++ app/src/main/java/com/example/homework16/login/LoginFragment.kt
@@ -0,0 +1,26 @@
+package com.example.homework16.login
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.homework16.BaseFragment
+import com.example.homework16.R
+import com.example.homework16.databinding.FragmentLoginBinding
+
+
+class LoginFragment : BaseFragment<FragmentLoginBinding>(FragmentLoginBinding::inflate) {
+    override fun setup() {
+
+    }
+
+    override fun setupListeners() {
+
+    }
+
+    override fun bindData() {
+
+    }
+
+}
---
+++ app/src/main/java/com/example/homework16/register/RegisterFragment.kt
@@ -0,0 +1,25 @@
+package com.example.homework16.register
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.homework16.BaseFragment
+import com.example.homework16.R
+import com.example.homework16.databinding.FragmentRegisterBinding
+
+class RegisterFragment : BaseFragment<FragmentRegisterBinding>(FragmentRegisterBinding::inflate) {
+    override fun setup() {
+
+    }
+
+    override fun setupListeners() {
+
+    }
+
+    override fun bindData() {
+
+    }
+
+}
