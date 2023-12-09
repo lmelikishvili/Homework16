6dad02f8faf0319a4ee51985127ce144ad90b488
lmelikishvili <levanimelikishvili@gmail.com>
2023-12-10 02:46:27 +0400

    before_logic

--- app/src/main/java/com/example/homework16/welcome/WelcomeFragment.kt
+++ app/src/main/java/com/example/homework16/welcome/WelcomeFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.example.homework16.BaseFragment
 import com.example.homework16.R
 import com.example.homework16.databinding.FragmentWelcomeBinding
@@ -15,7 +16,14 @@ class WelcomeFragment : BaseFragment<FragmentWelcomeBinding>(FragmentWelcomeBind
     }
 
     override fun setupListeners() {
-
+        with(binding){
+            btnRegister.setOnClickListener(){
+                findNavController().navigate(R.id.action_welcomeFragment_to_registerFragment)
+            }
+            btnLogin.setOnClickListener(){
+                findNavController().navigate(R.id.action_welcomeFragment_to_loginFragment)
+            }
+        }
     }
 
     override fun bindData() {
