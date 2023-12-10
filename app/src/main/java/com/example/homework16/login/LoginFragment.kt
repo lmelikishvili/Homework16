0e95e4d901d06bcacca5d0b87b5c9e8eb9baebee
lmelikishvili <levanimelikishvili@gmail.com>
2023-12-10 13:39:18 +0400

    witch_volley_before_retrofit

--- app/src/main/java/com/example/homework16/login/LoginFragment.kt
+++ app/src/main/java/com/example/homework16/login/LoginFragment.kt
@@ -5,9 +5,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import com.android.volley.Request
+import com.android.volley.VolleyLog
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
 import com.example.homework16.BaseFragment
 import com.example.homework16.R
 import com.example.homework16.databinding.FragmentLoginBinding
+import org.json.JSONObject
 
 
 class LoginFragment : BaseFragment<FragmentLoginBinding>(FragmentLoginBinding::inflate) {
@@ -16,9 +22,25 @@ class LoginFragment : BaseFragment<FragmentLoginBinding>(FragmentLoginBinding::i
     }
 
     override fun setupListeners() {
-
+        binding.btnLogin.setOnClickListener(){
+            val requsetParams = JSONObject()
+            requsetParams.put("email", binding.etEmail.text.toString())
+            requsetParams.put("password", binding.etPassword.text.toString())
+            val url = "https://reqres.in/api/login"
+            val queue = Volley.newRequestQueue(this.context)
+            val request = JsonObjectRequest(Request.Method.POST, url, requsetParams,{ response ->
+                try {
+                    VolleyLog.d("resultianting", "${response}")
+                    Toast.makeText(this.context, "${response}", Toast.LENGTH_LONG).show()
+                }catch (e: Exception){
+                    VolleyLog.d("resultiantingFail", "${e.message}")
+                }
+            }, {error ->
+                Toast.makeText(this.context, "Message Error", Toast.LENGTH_LONG).show()
+            })
+            queue.add(request)
+        }
     }
-
     override fun bindData() {
 
     }
--- app/src/main/java/com/example/homework16/register/RegisterFragment.kt
+++ app/src/main/java/com/example/homework16/register/RegisterFragment.kt
@@ -1,13 +1,19 @@
 package com.example.homework16.register
 
+import android.app.DownloadManager.Request
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import com.android.volley.VolleyLog.d
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
 import com.example.homework16.BaseFragment
 import com.example.homework16.R
 import com.example.homework16.databinding.FragmentRegisterBinding
+import org.json.JSONObject
 
 class RegisterFragment : BaseFragment<FragmentRegisterBinding>(FragmentRegisterBinding::inflate) {
     override fun setup() {
@@ -15,11 +21,26 @@ class RegisterFragment : BaseFragment<FragmentRegisterBinding>(FragmentRegisterB
     }
 
     override fun setupListeners() {
-
+        binding.btnRegister.setOnClickListener(){
+            val requsetParams = JSONObject()
+            requsetParams.put("email", binding.etEmail.text.toString())
+            requsetParams.put("password", binding.etPassword.text.toString())
+            val url = "https://reqres.in/api/register"
+            val queue = Volley.newRequestQueue(this.context)
+            val request = JsonObjectRequest(com.android.volley.Request.Method.POST, url, requsetParams,{response ->
+                try {
+                    d("resultianting", "${response}")
+                    Toast.makeText(this.context, "${response}", Toast.LENGTH_LONG).show()
+                }catch (e: Exception){
+                    d("resultiantingFail", "${e.message}")
+                }
+            }, {error ->
+                Toast.makeText(this.context, "Message Error", Toast.LENGTH_LONG).show()
+            })
+            queue.add(request)
+        }
     }
-
     override fun bindData() {
 
     }
-
 }
